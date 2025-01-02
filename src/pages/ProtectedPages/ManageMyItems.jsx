bf9deed8498ff645e59453cbad9d0dcf1cd5a899
MursalinDevOps <mursalin1768@gmail.com>
2025-01-03 00:13:31 +0600

    delete functionality added

--- src/pages/ProtectedPages/ManageMyItems.jsx
+++ src/pages/ProtectedPages/ManageMyItems.jsx
@@ -2,21 +2,59 @@ import { Link, useLoaderData, useNavigate } from "react-router-dom";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 import PageTitle from "../../components/PageTitle";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AuthContext from "../../context/AuthContext";
+import Swal from "sweetalert2";
 
 const ManageMyItems = () => {
-    const { user } = useContext(AuthContext);
+    const { user, setLoading } = useContext(AuthContext);
     const navigate = useNavigate();
     const postedItems = useLoaderData();
     const myPostedItems = postedItems.filter(item => item.contactEmail === user?.email);
+
+    const [items, setItems] = useState(myPostedItems); 
     // console.log(postedItems, myPostedItems)  
 
 
     const handleUpdate = (id) => {
-        if(!user){
+        if (!user) {
             navigate('/login')
         }
+    }
+    const handleDelete = (_id) => {
+        console.log('Deleting', _id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                setLoading(true);
+                fetch(`http://localhost:3000/all-items/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        setLoading(false);
+                        // console.log(data);
+                        if (data.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                            // const remaining = myPostedItems.filter((singleItem) => singleItem._id !== _id);
+                            // setItems(remaining);
+                            setItems(prevItems => prevItems.filter(item => item._id !== _id));
+
+                        }
+                    })
+            }
+        });
     }
     return (
         <div>
@@ -26,7 +64,7 @@ const ManageMyItems = () => {
             </nav>
             <section >
                 <div className="overflow-x-auto md:overflow-hidden rounded-lg w-full flex flex-col min-h-screen ">
-            <h2 className="text-2xl md:text-4xl font-bold mt-6 text-blue-700 text-center mb-10">My Items</h2>
+                    <h2 className="text-2xl md:text-4xl font-bold mt-6 text-blue-700 text-center mb-10">My Items</h2>
                     <table className="table lg:ml-28">
                         {/* head */}
                         <thead>
@@ -60,10 +98,10 @@ const ManageMyItems = () => {
                                     </td>
                                     <td>{postedItem.category}</td>
                                     <th>
-                                       <div className="flex gap-2">
-                                      <Link to={`/updateItems/${postedItem._id}`}><button onClick={()=>handleUpdate(postedItem._id)} className="btn text-white btn-success btn-sm">Update</button></Link>
-                                      <Link><button className="btn text-white btn-error btn-sm ml-3">Delete</button></Link>
-                                       </div>
+                                        <div className="flex gap-2">
+                                            <Link to={`/updateItems/${postedItem._id}`}><button onClick={() => handleUpdate(postedItem._id)} className="btn text-white btn-success btn-sm">Update</button></Link>
+                                            <Link><button onClick={()=>handleDelete(postedItem._id)} className="btn text-white btn-error btn-sm ml-3">Delete</button></Link>
+                                        </div>
                                     </th>
                                 </tr>)
                             }
